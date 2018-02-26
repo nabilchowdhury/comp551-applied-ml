37cef9b9ec49f05e859b9cdeb26c8a54806b10ef
nabilchowdhury <nabillionaire@live.com>
2018-02-26 02:13:27 -0500

    Code final

--- Assignment3/Q1.py
+++ Assignment3/Q1.py
@@ -1,5 +1,4 @@
 # Base libraries
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
@@ -7,13 +6,13 @@ import scipy
 
 # Processing & feature generation
 from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import f1_score
 from sklearn.model_selection import GridSearchCV, PredefinedSplit
 from sklearn.preprocessing import normalize
 
 # Classifiers
-from sklearn.naive_bayes import BernoulliNB, GaussianNB
+from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
@@ -34,19 +33,43 @@ IMDB:
     Type: 2 class problem (1: positive, 0: negative)
 '''
 
+# Constants
 PATH = r'./Datasets'
 M_FEATURES = 10000
 
 
 def read_dataset(filename, column_names=['Review', 'Label']):
+    '''
+    Parses the file into a pandas dataframe object.
+    :param filename: The file to be parsed
+    :param column_names: The desired column names of the dataframe
+    :return: The file as a dataframe object
+    '''
     return pd.read_table(os.path.join(PATH, filename), sep='\t', lineterminator='\n', header=None, names=column_names)
 
 
 def preprocess(dataframe, column):
+    '''
+    Preprocesses the reviews by stripping away all non-word, non-space characters. Additionally removes <br /> tags for IMDB set
+    :param dataframe: The dataframe object
+    :param column: The column to be preprocessed. This will be 'Review' for this assignment.
+    :return: None
+    '''
     dataframe[column] = dataframe[column].str.replace('<br /><br />', ' ').str.replace('[^\w\s]', '').str.lower()
 
 
 def get_vocabulary(training_dict, column, features=M_FEATURES, save_to_file=False):
+    '''
+    For each training set in training_dict, return the corresponding vocabulary to be used as the feature set.
+    The method first counts the frequencies of all words across each training set, and then chooses the top most
+    frequent words as the feature set.
+    :param training_dict: The dictionary of training sets. We have yelp and IMDB training sets.
+    :param column: The column to get the words from. This is 'Review'
+    :param features: The number of top most frequent features to be used
+    :param save_to_file: If True, saves the feature set, along with the frequencies and IDs to their corresponding file
+                         as required by the assignment
+    :return: Dictionary of vocabularies for both yelp and IMDB
+    '''
     most_common = {}
     for dataset in training_dict:
         all_words_list = [word for sentence in training_dict[dataset][column].str.split().tolist() for word in sentence]
@@ -63,19 +86,35 @@ def get_vocabulary(training_dict, column, features=M_FEATURES, save_to_file=Fals
 
 
 def bag_of_words(datasets, vocabulary, xname='Review', yname='Label'):
-    binary_bog = {}
-    freq_bog = {}
+    '''
+    Converts each dataset in datasets to both binary and frequency bag of words representations.
+    :param datasets: Dictionary of datasets to be converted
+    :param vocabulary: The vocabulary extracted by :func:`get_vocabulary`
+    :param xname: The name of the feature column ('Review')
+    :param yname: The name of the label column ('Label')
+    :return: Binary and frequency BoW dictionaries. Each dictionary has keys corresponding to the keys of datasets.
+    '''
+    binary_bow = {}
+    freq_bow = {}
     vectorizer = CountVectorizer(vocabulary=vocabulary)
     for name in datasets:
         vec = vectorizer.fit_transform(datasets[name][xname])
-        freq_bog[name] = [normalize(vec), datasets[name][yname]]
+        freq_bow[name] = [normalize(vec), datasets[name][yname]]
         vec[vec > 1] = 1
-        binary_bog[name] = [vec, datasets[name][yname]]
+        binary_bow[name] = [vec, datasets[name][yname]]
 
-    return binary_bog, freq_bog
+    return binary_bow, freq_bow
 
 
 def write_converted_dataset(datasets, vocab_dict, dataset_name):
+    '''
+    Replaces the words of the datasets by their unique IDs from the vocabulary, as required by the assignment. It then
+    writes the converted datasets to file.
+    :param datasets: Dictionary containing the datasets to be converted
+    :param vocab_dict: The vocabulary dictionary obtained from :func:`get_vocabulary`
+    :param dataset_name: 'yelp' or 'imdb'
+    :return: None
+    '''
     for dataset in datasets[dataset_name]:
         with open('./Submission/' + dataset_name + '-' + dataset + '.txt', 'w') as file:
             for i in range(len(datasets[dataset_name][dataset])):
@@ -84,40 +123,73 @@ def write_converted_dataset(datasets, vocab_dict, dataset_name):
 
 
 def random_classifier(train_on, predict_on):
+    '''
+    Classifies predict_on into a random class.
+    :param train_on: The training set
+    :param predict_on: The test set
+    :return: Predicted labels of the test set
+    '''
     return np.random.choice(np.unique(train_on[1]), len(predict_on[1]))
 
 
 def majority_class_classifier(train_on, predict_on):
+    '''
+    Classifies predict_on into the majority class (mode) of the training set.
+    :param train_on: The training set
+    :param predict_on: The test set
+    :return: Predicted labels of the test set
+    '''
     return np.full(len(predict_on[1]), scipy.stats.mode(train_on[1])[0][0])
 
 
 def print_format(s, *args):
+    '''
+    Helper to print formatted strings
+    :param s: The unformatted string with placeholders
+    :param args: The args that go into the placeholders of s
+    :return: None
+    '''
     print(s.format(*args))
 
 
-def do_clf_test(clf, dataset_dict, tune_params=None, average='micro'):
+def do_clf_test(clf, dataset_dict, tune_params=None, tune=True, average='micro'):
+    '''
+    Fits the classifier clf to the training set, and predicts on the test set. Prints the F1 scores for each set.
+    :param clf: An sklearn classifier or :func:`random_classifier` or :func:`majority_class_classifier`
+    :param dataset_dict: Dictionary of datasets to predict on (must contain keys 'train', 'valid', 'test')
+    :param tune_params: If not None, will tune the parameters on the validation set before predicting on test. Tuning
+                        is done using sklearn's GridSearchCV
+    :param tune: Must be true in addition to tune_params not being None to perform tuning
+    :param average: The average paramters of sklearn's f1_score. Defaults to 'micro'
+    :return: None
+    '''
     name = clf.__name__.upper() if callable(clf) else clf.__class__.__name__.upper()
+    print_format('\tScores using {}:', name)
+
     if not callable(clf):
-        if tune_params is not None:
+        train_x = dataset_dict['train'][0]
+        train_y = dataset_dict['train'][1]
+        if tune and tune_params is not None:
             # Set up GridSearch with validation set
-            total_length = len(dataset_dict['train'][1]) + len(dataset_dict['valid'][1])
-            ps = PredefinedSplit(test_fold=[-1 if i < len(dataset_dict['train'][1]) else 0 for i in range(total_length)])
-            clf = GridSearchCV(clf, tune_params, cv=ps, refit=True)
-            train_x = scipy.sparse.vstack([dataset_dict['train'][0], dataset_dict['valid'][0]])
-            train_y = np.concatenate([dataset_dict['train'][1], dataset_dict['valid'][1]])
-        else:
-            train_x = dataset_dict['train'][0]
-            train_y = dataset_dict['train'][1]
+            valid_x, valid_y = dataset_dict['valid'][0], dataset_dict['valid'][1]
+            ps = PredefinedSplit(test_fold=[-1 if i < len(train_y) else 0 for i in range(len(train_y) + len(valid_y))])
+            clf = GridSearchCV(clf, tune_params, cv=ps, n_jobs=2)
+            train_x = scipy.sparse.vstack([train_x, valid_x])
+            train_y = np.concatenate([train_y, valid_y])
 
         clf.fit(train_x, train_y)
+        if tune and tune_params is not None: print('\t\tBest params:', clf.best_params_)
 
     for dname, dset in dataset_dict.items():
         score = f1_score(dset[1], clf(dataset_dict['train'], dset) if callable(clf) else clf.predict(dset[0]), average=average)
-        print_format('\t{} score using {}: {}\n', dname.upper(), name, score)
+        print_format('\t\t{}: {}', dname.upper(), score)
 
 
 if __name__ == '__main__':
-    WRITE = False
+    # Useful flags
+    WRITE = False  # If true, write to file
+    PERFORM_TUNING = True
+
     # Yelp datsets
     yelp_train = read_dataset('yelp-train.txt')
     yelp_valid = read_dataset('yelp-valid.txt')
@@ -128,7 +200,7 @@ if __name__ == '__main__':
     imdb_valid = read_dataset('IMDB-valid.txt')
     imdb_test = read_dataset('IMDB-test.txt')
 
-    # yelp
+    # All sets
     datasets = {
         'yelp': {'train': yelp_train, 'valid': yelp_valid, 'test': yelp_test},
         'imdb': {'train': imdb_train, 'valid': imdb_valid, 'test': imdb_test}
@@ -170,24 +242,86 @@ if __name__ == '__main__':
         write_converted_dataset(datasets, vocabulary, 'imdb')
 
 
-    # run_predictions([random_classifier, majority_class_classifier, LinearSVC(), DecisionTreeClassifier()])
-
     '''
-    Question 2: Yelp binary bag of words with hyperparameter turning using GridSearchCV :)
+    Question 2: Question 2: Yelp Binary BoW f1 scores on random uniform classifier, majority-class classifier, 
+    Naive Bayes, Decision Trees, LinearSVM w/ hyperparameter tuning using GridSearchCV
     '''
     print('Yelp Binary Bag of Words Performances')
-    # do_clf_test(random_classifier, yelp_binary)
-    # do_clf_test(majority_class_classifier, yelp_binary)
-    do_clf_test(BernoulliNB(), yelp_binary, [{'alpha': [pow(10, -i) for i in range(6)]}])
-    # do_clf_test(DecisionTreeClassifier(), yelp_binary)
-    # do_clf_test(LinearSVC(), yelp_binary)
+    do_clf_test(random_classifier, yelp_binary)  # Random uniform classifier
+    do_clf_test(majority_class_classifier, yelp_binary)  # Majority class classifier
+
+    # BernoulliNB
+    params = {'alpha': np.arange(0.01, 1.01, 0.01)}
+    do_clf_test(BernoulliNB(), yelp_binary, params, tune=PERFORM_TUNING)
+
+    # Decision Tree
+    params = {'max_depth': np.arange(13, 17),
+              'max_features': np.arange(0.1, 0.5, 0.1),
+              'min_samples_leaf': np.arange(3, 6)}
+    do_clf_test(DecisionTreeClassifier(), yelp_binary, params, tune=PERFORM_TUNING)
+
+    # Linear SVM
+    params = {'C': np.logspace(-2, 2, num=8),
+              'max_iter': np.arange(10, 100, 10)}
+    do_clf_test(LinearSVC(), yelp_binary, params, tune=PERFORM_TUNING)
 
     '''
-    Question 3: Yelp frequency bag of words
+    Question 3: Yelp Frequency BoW
     '''
-    # print('Yelp Frequency Bag of Words Performances')
-    # do_clf_test(random_classifier, yelp_freq)
-    # do_clf_test(majority_class_classifier, yelp_freq)
-    # # do_clf_test(GaussianNB(), yelp_freq)
-    # do_clf_test(DecisionTreeClassifier(), yelp_freq)
-    # do_clf_test(LinearSVC(), yelp_freq)
+    print('Yelp Frequency Bag of Words Performances')
+    do_clf_test(random_classifier, yelp_freq)
+    do_clf_test(majority_class_classifier, yelp_freq)
+
+    # GaussianNB: Requires dense arrays
+    do_clf_test(GaussianNB(), {key: [value[0].toarray(), value[1]] for key, value in yelp_freq.items()})
+
+    # Decision Tree
+    params = {'max_depth': np.arange(13, 17),
+              'max_features': np.arange(0.1, 0.5, 0.1),
+              'min_samples_leaf': np.arange(3, 6)}
+    do_clf_test(DecisionTreeClassifier(), yelp_freq, params, tune=PERFORM_TUNING)
+
+    # Linear SVM
+    params = {'C': np.logspace(-2, 2, num=8),
+              'max_iter': np.arange(10, 100, 10)}
+    do_clf_test(LinearSVC(), yelp_freq, params, tune=PERFORM_TUNING)
+
+    '''
+    Question 4: Repeat Q2 and Q3 with IMDB
+    '''
+    print('IMDB Binary Bag of Words Performances')
+    do_clf_test(random_classifier, imdb_binary)
+    # Majority class classifier doesn't make sense for IMDB since it is a balanced dataset
+
+    # BernoulliNB
+    params = {'alpha': np.arange(0.01, 1.01, 0.01)}
+    do_clf_test(BernoulliNB(), imdb_binary, params, tune=PERFORM_TUNING)
+
+    # Decision Tree
+    params = {'max_depth': np.arange(13, 17),
+              'max_features': np.arange(0.1, 0.5, 0.1),
+              'min_samples_leaf': np.arange(3, 6)}
+    do_clf_test(DecisionTreeClassifier(), imdb_binary, params, tune=PERFORM_TUNING)
+
+    # Linear SVM
+    params = {'C': np.logspace(-2, 2, num=8),
+              'max_iter': np.arange(10, 100, 10)}
+    do_clf_test(LinearSVC(), imdb_binary, params, tune=PERFORM_TUNING)
+
+    print('IMDB Frequency Bag of Words Performances')
+    do_clf_test(random_classifier, imdb_freq)
+
+    # GaussianNB: Requires dense arrays
+    # No parameters to tune
+    do_clf_test(GaussianNB(), {key: [value[0].toarray(), value[1]] for key, value in imdb_freq.items()})
+
+    # Decision Tree
+    params = {'max_depth': np.arange(13, 17),
+              'max_features': np.arange(0.1, 0.5, 0.1),
+              'min_samples_leaf': np.arange(3, 6)}
+    do_clf_test(DecisionTreeClassifier(), imdb_freq, params, tune=PERFORM_TUNING)
+
+    # Linear SVM
+    params = {'C': np.logspace(-2, 2, num=8),
+              'max_iter': np.arange(10, 100, 10)}
+    do_clf_test(LinearSVC(), imdb_freq, params, tune=PERFORM_TUNING)
